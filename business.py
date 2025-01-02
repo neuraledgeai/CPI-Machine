3e94a8597b4e627dd3320146da4cc44415c831d0
Anoop Kumar U <64458507+neuraledgeai@users.noreply.github.com>
2025-01-02 16:31:11 +0530

    Update business.py

--- business.py
+++ business.py
@@ -109,7 +109,7 @@ class Model:
         df,
         x=df.index,
         y="CCPI",
-        title=(""),
+        title=(" "),
         labels={
             "CCPI": "CPI (Index 1982-1984=100)",
             "index": "Year"
@@ -122,7 +122,7 @@ class Model:
         df,
         x=df.index,
         y="CPI_PC",
-        title="",
+        title=(" "),
         labels={"CPI_PC": "CPI % Change a Year Ago", "index": "Year"},
         template="plotly_white"
     )
@@ -132,7 +132,7 @@ class Model:
         df,
         x=df.index,
         y="CCPI_PC",
-        title="",
+        title=(" "),
         labels={"CCPI_PC": "CPI % Change a Year Ago", "index": "Year"},
         template="plotly_white"
     )
